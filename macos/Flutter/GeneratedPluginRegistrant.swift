1aed93d7bdbcbf6a2952403b9ad5fac5c07b6f3c
n1ghtr33x <dima.luts.standoff@gmail.com>
2025-07-12 21:47:52 +0200

    Replace window_manager with Firebase plugins
    Removed window_manager and screen_retriever plugins from all platforms and added firebase_core and firebase_messaging dependencies. Updated plugin registrant files and configuration to support Firebase integration. Added GoogleService-Info.plist for iOS Firebase setup.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import screen_retriever
+import firebase_core
+import firebase_messaging
 import shared_preferences_foundation
-import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,8 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  screen_retriever
-  window_manager
+  firebase_core
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
